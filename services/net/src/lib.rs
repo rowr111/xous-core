f49636242662b8fe6e7348d56c5fa69270b98a08
Jeanie <rowr111@users.noreply.github.com>
2024-12-22 16:01:04 +0900

    starting point

--- services/net/src/lib.rs
+++ services/net/src/lib.rs
@@ -63,6 +63,27 @@ impl NetManager {
         }
     }
 
+    /// Sets the debug level for the network connection.
+    ///
+    /// This function sends a message to the network server to set the debug level. The debug level
+    /// determines the verbosity of the logging output. The supported levels are:
+    /// - `log::LevelFilter::Info`: Sets the debug level to Info.
+    /// - `log::LevelFilter::Debug`: Sets the debug level to Debug.
+    /// - `log::LevelFilter::Trace`: Sets the debug level to Trace.
+    /// Any other level will default to Info.
+    ///
+    /// # Arguments
+    ///
+    /// * `level` - The desired debug level as a `log::LevelFilter`.
+    ///
+    /// # Example
+    /// ```
+    /// use log::LevelFilter;
+    /// use net::Net; // Assuming `Net` is the struct that contains `set_debug_level`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// net.set_debug_level(LevelFilter::Debug); // Set the debug level to Debug
+    /// ```
     pub fn set_debug_level(&self, level: log::LevelFilter) {
         let code = match level {
             log::LevelFilter::Info => 0,
@@ -77,6 +98,28 @@ impl NetManager {
         .expect("couldn't set debug");
     }
 
+    /// Retrieves the IPv4 configuration for the network connection.
+    ///
+    /// This function sends a message to the network server to get the current IPv4 configuration.
+    /// The configuration is returned as an `Option<Ipv4Conf>`, where `None` indicates that the
+    /// configuration could not be retrieved.
+    ///
+    /// # Returns
+    ///
+    /// * `Option<Ipv4Conf>` - The current IPv4 configuration, or `None` if the configuration could not be
+    ///   retrieved.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `get_ipv4_config`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// if let Some(config) = net.get_ipv4_config() {
+    ///     println!("IPv4 Configuration: {:?}", config);
+    /// } else {
+    ///     println!("Failed to retrieve IPv4 configuration");
+    /// }
+    /// ```
     pub fn get_ipv4_config(&self) -> Option<Ipv4Conf> {
         let storage = Some(Ipv4Conf::default().encode_u16());
         let mut buf = Buffer::into_buf(storage).expect("Couldn't convert to memory structure");
@@ -91,6 +134,19 @@ impl NetManager {
         }
     }
 
+    /// Resets the network connection.
+    ///
+    /// This function sends a message to the network server to reset the network connection. The reset
+    /// operation is blocking, meaning the function will wait until the network server confirms that the
+    /// reset has been completed.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `reset`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// net.reset(); // Reset the network connection
+    /// ```
     pub fn reset(&self) {
         send_message(
             self.netconn.conn(),
@@ -99,9 +155,37 @@ impl NetManager {
         .expect("couldn't send reset");
     }
 
-    /// This is the function that the system should be using to check the wifi state -- it will read
-    /// the cached value from the connection manager. The direct call to the COM could cause too much
+    /// Subscribes to WiFi state updates.
+    ///
+    /// This function sets up a subscription to receive WiFi state updates from the connection manager.
+    /// It creates a one-time server to handle the subscription and forwards the updates to the specified
+    /// return connection ID and opcode. This function should be used to check the WiFi state by reading
+    /// the cached value from the connection manager, as direct calls to the COM could cause too much
     /// congestion.
+    ///
+    /// # Arguments
+    ///
+    /// * `return_cid` - The connection ID to which the WiFi state updates should be forwarded.
+    /// * `opcode` - The opcode to use when forwarding the WiFi state updates.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), xous::Error>` - Returns `Ok(())` if the subscription is set up successfully, or an error
+    ///   if it fails.
+    ///
+    /// # Example
+    /// ```
+    /// use xous::{CID, connect, create_server};
+    /// use net::Net; // Assuming `Net` is the struct that contains `wifi_state_subscribe`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// let return_cid = connect(create_server().unwrap()).unwrap();
+    /// let opcode = 1234; // Example opcode
+    /// match net.wifi_state_subscribe(return_cid, opcode) {
+    ///     Ok(()) => println!("Subscribed to WiFi state updates successfully"),
+    ///     Err(e) => eprintln!("Failed to subscribe to WiFi state updates: {:?}", e),
+    /// }
+    /// ```
     pub fn wifi_state_subscribe(&mut self, return_cid: CID, opcode: u32) -> Result<(), xous::Error> {
         if self.wifi_state_cid.is_none() {
             let onetime_sid = xous::create_server().unwrap();
@@ -149,7 +233,27 @@ impl NetManager {
         }
     }
 
-    /// If we're not already subscribed, returns without error.
+    /// Unsubscribes from WiFi state updates.
+    ///
+    /// This function unsubscribes from WiFi state updates by sending a message to the network server.
+    /// If the subscription exists, it will be removed and the associated resources will be cleaned up.
+    /// If there is no existing subscription, the function returns without error.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), xous::Error>` - Returns `Ok(())` if the unsubscription is successful, or an error if it
+    ///   fails.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `wifi_state_unsubscribe`
+    ///
+    /// let mut net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// match net.wifi_state_unsubscribe() {
+    ///     Ok(()) => println!("Unsubscribed from WiFi state updates successfully"),
+    ///     Err(e) => eprintln!("Failed to unsubscribe from WiFi state updates: {:?}", e),
+    /// }
+    /// ```
     pub fn wifi_state_unsubscribe(&mut self) -> Result<(), xous::Error> {
         if let Some(handler) = self.wifi_state_cid.take() {
             if let Some(sid) = self.wifi_state_sid.take() {
@@ -176,6 +280,29 @@ impl NetManager {
         Ok(())
     }
 
+    /// Retrieves the list of available WiFi SSIDs.
+    ///
+    /// This function sends a message to the network server to fetch the list of available WiFi SSIDs.
+    /// The function returns a tuple containing a vector of `SsidRecord` and the current `ScanState`.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(Vec<SsidRecord>, ScanState), xous::Error>` - Returns a tuple containing a vector of
+    ///   `SsidRecord` and the current `ScanState` if successful, or an error if it fails.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `wifi_get_ssid_list`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// match net.wifi_get_ssid_list() {
+    ///     Ok((ssid_list, scan_state)) => {
+    ///         println!("SSID List: {:?}", ssid_list);
+    ///         println!("Scan State: {:?}", scan_state);
+    ///     }
+    ///     Err(e) => eprintln!("Failed to retrieve SSID list: {:?}", e),
+    /// }
+    /// ```
     pub fn wifi_get_ssid_list(&self) -> Result<(Vec<SsidRecord>, ScanState), xous::Error> {
         let alloc = SsidList::default();
         let mut buf = Buffer::into_buf(alloc).map_err(|_| xous::Error::InternalError)?;
@@ -190,6 +317,27 @@ impl NetManager {
         Ok((ret, ssid_list.state))
     }
 
+    /// Stops the connection manager.
+    ///
+    /// This function sends a message to the network server to stop the connection manager. The connection
+    /// manager is responsible for managing network connections, and stopping it will halt all network
+    /// activities.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), xous::Error>` - Returns `Ok(())` if the connection manager is stopped successfully, or
+    ///   an error if it fails.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `connection_manager_stop`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// match net.connection_manager_stop() {
+    ///     Ok(()) => println!("Connection manager stopped successfully"),
+    ///     Err(e) => eprintln!("Failed to stop connection manager: {:?}", e),
+    /// }
+    /// ```
     pub fn connection_manager_stop(&self) -> Result<(), xous::Error> {
         send_message(
             self.netconn.conn(),
@@ -198,6 +346,27 @@ impl NetManager {
         .map(|_| ())
     }
 
+    /// Starts the connection manager.
+    ///
+    /// This function sends a message to the network server to start the connection manager. The connection
+    /// manager is responsible for managing network connections, and starting it will enable network
+    /// activities.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), xous::Error>` - Returns `Ok(())` if the connection manager is started successfully, or
+    ///   an error if it fails.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `connection_manager_run`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// match net.connection_manager_run() {
+    ///     Ok(()) => println!("Connection manager started successfully"),
+    ///     Err(e) => eprintln!("Failed to start connection manager: {:?}", e),
+    /// }
+    /// ```
     pub fn connection_manager_run(&self) -> Result<(), xous::Error> {
         send_message(
             self.netconn.conn(),
@@ -206,6 +375,27 @@ impl NetManager {
         .map(|_| ())
     }
 
+    /// Turns off WiFi and stops the connection manager.
+    ///
+    /// This function sends a message to the network server to turn off WiFi and stop the connection manager.
+    /// The connection manager is responsible for managing network connections, and stopping it will halt all
+    /// network activities.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), xous::Error>` - Returns `Ok(())` if the operation is successful, or an error if it
+    ///   fails.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `connection_manager_wifi_off_and_stop`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// match net.connection_manager_wifi_off_and_stop() {
+    ///     Ok(()) => println!("WiFi turned off and connection manager stopped successfully"),
+    ///     Err(e) => eprintln!("Failed to turn off WiFi and stop connection manager: {:?}", e),
+    /// }
+    /// ```
     pub fn connection_manager_wifi_off_and_stop(&self) -> Result<(), xous::Error> {
         send_message(
             self.netconn.conn(),
@@ -214,6 +404,27 @@ impl NetManager {
         .map(|_| ())
     }
 
+    /// Turns on WiFi and starts the connection manager.
+    ///
+    /// This function sends a message to the network server to turn on WiFi and start the connection manager.
+    /// The connection manager is responsible for managing network connections, and starting it will enable
+    /// network activities.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), xous::Error>` - Returns `Ok(())` if the operation is successful, or an error if it
+    ///   fails.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `connection_manager_wifi_on_and_run`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// match net.connection_manager_wifi_on_and_run() {
+    ///     Ok(()) => println!("WiFi turned on and connection manager started successfully"),
+    ///     Err(e) => eprintln!("Failed to turn on WiFi and start connection manager: {:?}", e),
+    /// }
+    /// ```
     pub fn connection_manager_wifi_on_and_run(&self) -> Result<(), xous::Error> {
         send_message(
             self.netconn.conn(),
@@ -222,6 +433,26 @@ impl NetManager {
         .map(|_| ())
     }
 
+    /// Turns on WiFi.
+    ///
+    /// This function sends a message to the network server to turn on WiFi. This will enable WiFi
+    /// connectivity without starting the connection manager.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), xous::Error>` - Returns `Ok(())` if the operation is successful, or an error if it
+    ///   fails.
+    ///
+    /// # Example
+    /// ```
+    /// use net::Net; // Assuming `Net` is the struct that contains `connection_manager_wifi_on`
+    ///
+    /// let net = Net::new(); // Assuming `Net::new()` initializes the network connection
+    /// match net.connection_manager_wifi_on() {
+    ///     Ok(()) => println!("WiFi turned on successfully"),
+    ///     Err(e) => eprintln!("Failed to turn on WiFi: {:?}", e),
+    /// }
+    /// ```
     pub fn connection_manager_wifi_on(&self) -> Result<(), xous::Error> {
         send_message(
             self.netconn.conn(),
